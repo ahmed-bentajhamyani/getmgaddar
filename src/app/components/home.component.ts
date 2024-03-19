9381c3cbcf258ad12d3b21f400ca92a66a28ca99
Ahmed Bentaj Hamyani <abentajhamyani@smb.sqli.com>
2024-03-19 22:09:35 +0000

    responsive home page

--- src/app/components/home.component.ts
+++ src/app/components/home.component.ts
@@ -3,17 +3,15 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'mg-home',
     template: `
-    <div class="absolute inset-0 bg-cover bg-center bg-black" style="background-image: url('assets/cover.jpg');"></div>
-    <div class="absolute inset-0 flex flex-col justify-center items-center text-white">
-        <div class="w-1/2 flex flex-col justify-center items-center">
-            <p class="text-8xl font-extrabold text-center uppercase">Find your strength</p>
-            <!-- <p class="text-2xl font-bold text-center uppercase">Inside and out...</p> -->
-            <div class="flex justify-center items-center space-x-4 w-full mt-10">
-                <a routerLink="auth/login"
-                    class="w-1/4 bg-rose-500 text-white text-center shadow-md py-4 rounded-md cursor-pointer select-none hover:bg-rose-700 transition-all ease-in-out duration-500">Login</a>
-                <a routerLink="auth/signup"
-                    class="w-1/4 border-2 border-rose-500 hover:bg-rose-500 text-white text-center shadow-md py-[14px] rounded-md cursor-pointer select-none transition-all ease-in-out duration-500">Sign
-                    Up</a>
+    <div class="relative h-screen">
+        <div class="absolute inset-0 bg-cover bg-center bg-black" style="background-image: url('assets/cover.jpg');"></div>
+        <div class="absolute inset-0 flex justify-center items-center">
+            <div class="w-full max-w-2xl text-white">
+                <p class="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-8xl font-extrabold text-center uppercase">Find your strength</p>
+                <div class="flex flex-col md:flex-row justify-center items-center space-y-4 md:space-y-0 md:space-x-4 mt-10">
+                    <a href="auth/login" class="w-1/3 md:w-1/4 bg-rose-500 text-white text-center shadow-md py-4 rounded-md cursor-pointer select-none hover:bg-rose-700 transition-all ease-in-out duration-500">Login</a>
+                    <a href="auth/signup" class="w-1/3 md:w-1/4 border-2 border-rose-500 hover:bg-rose-500 text-white text-center shadow-md mt-4 md:mt-0 py-[14px] rounded-md cursor-pointer select-none transition-all ease-in-out duration-500">Sign Up</a>
+                </div>
             </div>
         </div>
     </div>
